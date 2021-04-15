3a178e42f06c4da933e7887ea3553a8cb5c0ef09
Evelyn Pei <pei.evelyn.s@gmail.com>
2021-04-15 12:27:50 -0700

    edited some styling of components

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -17,6 +17,7 @@ class App extends React.Component {
       },
     };
     this.setView = this.setView.bind(this);
+    this.determineView = this.determineView.bind(this);
   }
 
   setView(name, params) {
@@ -28,17 +29,53 @@ class App extends React.Component {
     });
   }
 
+  determineView() {
+    let body = null;
 
+    switch (this.state.view.name) {
+      case 'homepage':
+        body = (
+          <>
+            <h1>Let's grab a drink.</h1>
+            <div className="button-container">
+              <button className="">STAY IN</button>
+              <button className="">GO OUT</button>
+            </div>
+          </>
+        );
+        break;
+      // case 'search-breweries':
+      //   body = (
+
+      //   );
+      //   break;
+      // case 'search-recipes':
+      //   body = (
+
+      //   );
+      //   break;
+      // case 'list-breweries':
+      //   body = (
+
+      //   );
+      //   break;
+      // case 'list-recipes':
+      //   body = (
+
+      //   );
+      //   break;
+    }
+
+    return body;
+  }
 
   render() {
+    const bodyContent = this.determineView();
+
     return (
       <div className={`${this.state.view.name} container`}>
         <Header />
-        <h1>Feeling thirsty?</h1>
-        <div className="button-container">
-          <button className="">Yes</button>
-          <button className="">No</button>
-        </div>
+        {bodyContent}
       </div>
     )
   }
