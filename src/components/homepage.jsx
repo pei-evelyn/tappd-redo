1806b870a95ba98f8e7a44ccf98501fa90fdccc5
Evelyn Pei <pei.evelyn.s@gmail.com>
2021-04-17 01:11:44 -0700

    Created new components and added functionality to breweryform

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -2,23 +2,23 @@ import React from 'react';
 
 function Homepage(props) {
   return (
-    <>
-      <h1 className="text-center">Let's grab a drink.</h1>
+    <div>
+      <h1 className="">Let's grab a drink.</h1>
       <div className="button-container">
         <button
           className="btn btn-lg btn-secondary"
-          onClick={() => this.props.setView('search-recipes', {})}
+          onClick={() => props.setView('search-recipes', {})}
         >
           STAY IN
         </button>
         <button
           className="btn btn-lg btn-secondary"
-          onClick={() => this.props.setView('search-breweries', {})}
+          onClick={() => props.setView('search-breweries', {})}
         >
           GO OUT
         </button>
       </div>
-    </>
+    </div>
   );
 }
 
